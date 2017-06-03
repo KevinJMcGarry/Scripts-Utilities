1a010a6d8bbac0811232b7fa62be74d665350a77
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-06-03 12:55:14 -0500

    minor optimizations

--- file-related/line-reordering.py
+++ file-related/line-reordering.py
@@ -1,4 +1,4 @@
-# Script to reorder every fourth line with every fifth line
+# Script to reorder/swap every fourth and fifth line in the file
 
 with open('pathToFile', 'r') as inputfile:
     with open('pathToFile', 'a') as outputfile:
@@ -6,8 +6,8 @@ with open('pathToFile', 'r') as inputfile:
         for line in inputfile:
             lines.append(line)
             if len(lines) == 5:  # writing the re-ordered lines to a new file
-                outputfile.write(lines[0] + lines[1] + lines[2] + lines[3] + line[5] + line[4])
-                lines = []  # flushing the array to repopulate with next 3 lines
+                outputfile.write(lines[0] + lines[1] + lines[2] + lines[4] + line[3])
+                lines = []  # flushing the array to repopulate with next 5 lines
         if len(lines) > 0:  # conditional for lines left over
             linesLeft = len(lines)
             for i in range(linesLeft):
