9165987fa8947170c61dd076e48cb495e365c1ca
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-06-03 12:47:57 -0500

    minor optimizations

--- file-related/line-reordering.py
+++ file-related/line-reordering.py
@@ -6,7 +6,7 @@ with open('pathToFile', 'r') as inputfile:
         for line in inputfile:
             lines.append(line)
             if len(lines) == 5:  # writing the re-ordered lines to a new file
-                outputfile.write(lines[0] + lines[1]+ lines[2] + lines[3] + line[5] + line[4])
+                outputfile.write(lines[0] + lines[1] + lines[2] + lines[3] + line[5] + line[4])
                 lines = []  # flushing the array to repopulate with next 3 lines
         if len(lines) > 0:  # conditional for lines left over
             linesLeft = len(lines)
