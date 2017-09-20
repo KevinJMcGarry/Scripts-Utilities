24ab2addee2a88fd3044c85b86b16e9d827e2efa
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-09-20 18:16:47 -0500

    cleaned up code, added more verbose output at end

--- file-related/ocr_tree.py
+++ file-related/ocr_tree.py
@@ -17,6 +17,7 @@ import subprocess
 import sys
 import time
 
+os.chdir('../../temp9/')
 print(os.getcwd())
 
 scriptDir = os.path.dirname(os.path.realpath(__file__))
@@ -24,12 +25,12 @@ print(scriptDir + '/ocr_tree.py: Start')
 
 time.sleep(5)
 
-if len(sys.argv) > 1:  # first argument/parameter option to specify is path of folder to 'walk'. Value assigned to startDirectory
+if len(sys.argv) > 1:  # first parameter option to specify is path of folder to 'walk'. value assigned to startDirectory
     startDirectory = sys.argv[1]
 else:
     startDirectory = '.'  # default directory to use if one isn't specified at runtime
 
-if len(sys.argv) > 2:  # second argument/arameter option could be path to a log file
+if len(sys.argv) > 2:  # second parameter option could be path to a log file
     logFile = sys.argv[2]
 else:
     logFile = scriptDir + '/ocr_tree.log'  # default log file to use if one isn't specified at runtime
@@ -38,23 +39,23 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', filena
 
 for directoryName, subdirectoryList, fileList in os.walk(startDirectory):
     print(directoryName, subdirectoryList, fileList)
+    time.sleep(5)
     logging.info('\n')
     logging.info(directoryName + '\n')
-    # os.chdir(directoryName)
     for eachFileName in fileList:
         fileExtension = os.path.splitext(eachFileName)[1]  # make sure this is splitting on last .
         if fileExtension == '.pdf':
             baseFileName = os.path.splitext(eachFileName)[0]
-            fullPath = directoryName + '/' + eachFileName
             ocrCommand = [r"ocrmypdf",  "--deskew", os.path.join(directoryName, eachFileName),
                           os.path.join(directoryName, baseFileName + '-ocr.pdf')]  # os.path.join to traverse all subdirs
             logging.info(ocrCommand)
-            proc = subprocess.Popen(ocrCommand, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            proc = subprocess.Popen(ocrCommand, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)  # stdout stored in proc
+            # stdout data from the Popen 'ocrCommand' is stored in proc variable
             result = str(proc.stdout.read())  # needed to cast this to string as subprocess returns bytes objects
             # print(result)
             if 'page already has text' in result:  # checking to see if file has already been ocr'd
-                result = 'Skipped document because it already contains searchable text'
+                result = 'Skipped the file "{}" because it already contains searchable text'.format(eachFileName)
                 print(result)
             else:
-                print('OCR complete')
+                print('OCR of file "{}" complete'.format(eachFileName))
             logging.info(result)
