d33c1601e03bf00771bae566c0a00e501277cd67
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-09-28 22:23:43 -0500

    initial commit of cleaning and archiving tool

---
+++ file-related/file_clean_archive.py
@@ -0,0 +1,27 @@
+'''
+script to take user input, scan folder, iterating over the first n lines of each file, applying
+regex substitution, writing cleaned lines to a new file in a separate folder and finally taring
+up the original files and storing them in a separate source folder with a date/timestamp for the name
+'''
+
+import os, re, tarfile
+
+def cleanfiles(folder, numoflines):
+    print(os.getcwd())
+    counter = 1
+    cleanedfolder = "cleanedfiles"  # specifying folder location of where to store the modified files
+    if not os.path.exists(os.path.join(folder, cleanedfolder)):
+        os.mkdir(os.path.join(folder, cleanedfolder))  # makes a folder called 'cleanedfiles' under folder path
+    for eachfile in os.listdir(folder):
+        if os.path.isfile(os.path.join(folder, eachfile)):  # only work on files, not folders
+            print(eachfile)
+            with open(eachfile, 'r') as inputfile:
+                with open(os.path.join(folder, cleanedfolder, eachfile + '.new'), 'w') as outputfile:
+                    for line in inputfile:
+                        if counter > 5:
+                            break
+                        re.sub("chaka", "foobar", line)
+                        outputfile.write(line)
+                        counter += 1
+                        
+# todo - add in datetime for new file. logic to delete .DS files
