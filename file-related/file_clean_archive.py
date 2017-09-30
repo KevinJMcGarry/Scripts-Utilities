375190b2d57a8ed7910b55923e4e789169c1ab36
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-09-30 17:44:14 -0500

    added zip component for archiving

--- file-related/file_clean_archive.py
+++ file-related/file_clean_archive.py
@@ -4,26 +4,35 @@ regex substitution, writing cleaned lines to a new file in a separate folder and
 up the original files and storing them in a separate source folder with a date/timestamp for the name
 '''
 
-import os, re, datetime, tarfile
+import os, re, datetime, gzip, shutil, tarfile
 
 def cleanfiles(folder, numoflines):
     print(os.getcwd())
     counter = 1
     cleanedfolder = "cleanedfiles"  # specifying folder location of where to store the modified files
+    archivedfolder = "archives"
     if not os.path.exists(os.path.join(folder, cleanedfolder)):
         os.mkdir(os.path.join(folder, cleanedfolder))  # makes a folder called 'cleanedfiles' under folder path
     for eachfile in os.listdir(folder):
+        print(eachfile)
         if eachfile.endswith("DS_Store"):
             continue  # skip mac created .DS_Store files
         if os.path.isfile(os.path.join(folder, eachfile)):  # only work on files, not folders
-            print('processing file - {} ...'.format(eachfile))
-            with open(eachfile, 'r') as inputfile:
+            print('processing file ... {} ...'.format(eachfile))
+            with open(os.path.join(folder, eachfile), 'r') as inputfile:
                 #with open(os.path.join(folder, cleanedfolder, eachfile + '.new'), 'w') as outputfile:
-                with open(os.path.join(folder, cleanedfolder, (datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S'))
-                        + ".txt"), 'w') as outputfile:
+                with open(os.path.join(folder, cleanedfolder, eachfile + "_" +
+                        (datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')) + ".txt"), 'w') as outputfile:
                     for line in inputfile:
                         if counter > 5:  # only working with the first 5 lines of each file
                             break
                         re.sub("chaka", "foo", line)  # adjust strings to substitute accordingly
                         outputfile.write(line)
                         counter += 1
+            if not os.path.exists(os.path.join(folder, archivedfolder)):
+                os.mkdir(os.path.join(folder, archivedfolder))
+            zip_file_name = eachfile + '.zip'
+            zip_file_path = os.path.join(folder, archivedfolder, zip_file_name)
+            with open(os.path.join(folder, eachfile), 'rb') as f_in:
+                with gzip.open(os.path.join(folder, archivedfolder, eachfile + '.gz'), 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
