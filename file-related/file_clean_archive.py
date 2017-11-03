5ad4c8819a97fbabbf5ebf0f767928fde6d04b8d
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-11-03 08:07:10 -0500

    using variables for os.joins for cleaner code

--- file-related/file_clean_archive.py
+++ file-related/file_clean_archive.py
@@ -7,19 +7,23 @@ up the original files and storing them in a separate source folder with a date/t
 import os, re, datetime, gzip, shutil
 
 def cleanfiles(folder, numoflines):
-    print(os.getcwd())
-    counter = 1
+
     cleanedfolder = "cleanedfiles"  # specifying folder location of where to store the modified files
     archivedfolder = "archives"
-    if not os.path.exists(os.path.join(folder, cleanedfolder)):
-        os.mkdir(os.path.join(folder, cleanedfolder))  # makes a folder called 'cleanedfiles' under specified folder path
+    cleanedFolderPath = os.path.join(folder, cleanedfolder)
+    archivedFolderPath = os.path.join(folder, archivedfolder)
+
+    if not os.path.exists(cleanedFolderPath):
+        os.mkdir(cleanedFolderPath)  # makes a folder called 'cleanedfiles' under specified folder path
     for eachfile in os.listdir(folder):
+        eachFilePath = os.path.join(folder, eachfile)
+        counter = 1
         if eachfile.endswith("DS_Store"):
             continue  # skip mac created .DS_Store files
-        if os.path.isfile(os.path.join(folder, eachfile)):  # only work on files, not folders
+        if os.path.isfile(eachFilePath):  # only work on files, not folders
             print('processing file ... {} ...'.format(eachfile))
-            with open(os.path.join(folder, eachfile), 'r') as inputfile:
-                with open(os.path.join(folder, cleanedfolder, eachfile + "_" +
+            with open(os.path.join(eachFilePath), 'r') as inputfile:
+                with open(os.path.join(cleanedFolderPath, eachfile + "_" +
                         (datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')) + ".txt"), 'w') as outputfile:
                     for line in inputfile:
                         if counter > numoflines:  # only working with the first 5 lines of each file
@@ -27,11 +31,11 @@ def cleanfiles(folder, numoflines):
                         re.sub("chaka", "foo", line)  # adjust strings to substitute accordingly
                         outputfile.write(line)
                         counter += 1
-            if not os.path.exists(os.path.join(folder, archivedfolder)):
-                os.mkdir(os.path.join(folder, archivedfolder))
+            if not os.path.exists(archivedFolderPath):
+                os.mkdir(archivedFolderPath)
             zip_file_name = eachfile + '.zip'
-            zip_file_path = os.path.join(folder, archivedfolder, zip_file_name)
-            with open(os.path.join(folder, eachfile), 'rb') as f_in:
-                with gzip.open(os.path.join(folder, archivedfolder, eachfile + '.gz'), 'wb') as f_out:
+            zip_file_path = os.path.join(archivedFolderPath, zip_file_name)
+            with open(os.path.join(eachFilePath), 'rb') as f_in:
+                with gzip.open(os.path.join(archivedFolderPath, eachfile + '.gz'), 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
-            os.remove(os.path.join(folder, eachfile))  # delete the original files after archive complete
+            os.remove(os.path.join(eachFilePath))  # delete the original files after archive complete
