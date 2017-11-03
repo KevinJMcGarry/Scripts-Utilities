ae951bc86f8af64624088da33127fd2570f80a28
Kevin McGarry <kevinjmcgarry@gmail.com>
2017-11-03 08:46:50 -0500

    added command line arguments and help message

--- file-related/file_clean_archive.py
+++ file-related/file_clean_archive.py
@@ -2,9 +2,27 @@
 script to take user input, scan folder, iterating over the first n lines of each file, applying
 regex substitution, writing cleaned lines to a new file in a separate folder and finally taring
 up the original files and storing them in a separate source folder with a date/timestamp for the name
+
+Usage:
+
+python3 file_clean_archive
 '''
 
-import os, re, datetime, gzip, shutil
+import os, re, datetime, gzip, shutil, sys
+
+if len(sys.argv) < 3:
+    print("\n*****************************************************************************")
+    print("you must furnish the folder path and number of lines arguments")
+    print("example - python3 file_clean_archive.py \"/Users/kevinmcgarry/foldername\" 7")
+    print("be sure to use quotes around the full folder path")
+    print("*****************************************************************************\n")
+    exit()
+else:
+    folder = sys.argv[1]
+    numoflines = int(sys.argv[2])  # casting to int for counter operation
+
+print("\nprocessing file in folder {} ... \n".format(folder))
+
 
 def cleanfiles(folder, numoflines):
 
@@ -26,7 +44,7 @@ def cleanfiles(folder, numoflines):
                 with open(os.path.join(cleanedFolderPath, eachfile + "_" +
                         (datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')) + ".txt"), 'w') as outputfile:
                     for line in inputfile:
-                        if counter > numoflines:  # only working with the first 5 lines of each file
+                        if counter > numoflines:  # only working with the first n lines of each file
                             break
                         re.sub("chaka", "foo", line)  # adjust strings to substitute accordingly
                         outputfile.write(line)
@@ -39,3 +57,5 @@ def cleanfiles(folder, numoflines):
                 with gzip.open(os.path.join(archivedFolderPath, eachfile + '.gz'), 'wb') as f_out:
                     shutil.copyfileobj(f_in, f_out)
             os.remove(os.path.join(eachFilePath))  # delete the original files after archive complete
+
+cleanfiles(folder, numoflines)
