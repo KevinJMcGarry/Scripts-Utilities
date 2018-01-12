3556603db33ca4c7a11a6c8dfa3c524ab4c3d3f4
Kevin McGarry <kevinjmcgarry@gmail.com>
2018-01-12 08:55:12 -0600

    updated to include count of all words, not just lines

--- file-related/find_strings.py
+++ file-related/find_strings.py
@@ -15,13 +15,15 @@ def findstring():
     for this_dir, dir_names, file_names in os.walk(startDirectory):
         for eachFile in file_names:
             fpath = os.path.join(this_dir, eachFile)
-            with open(fpath, 'rb') as inputfile:
+            with open(fpath, 'r') as inputfile:
                 count = 0  # reset count when opening new file
                 line_hits = []  # reset list when opening new file
                 for line in inputfile:
-                    if wordOfInterest in str(line):
-                        count += 1
-                        line_hits.append(line)  # append all file's lines that contain the string
+                    words = line.split()
+                    for word in words:
+                        if word == wordOfInterest:
+                            count += 1
+                            line_hits.append(line)  # append all file's lines that contain the string
             if count > 0:
                 print("\n found the word \'{}\' {} times in file \'{}\'".format(wordOfInterest, count, fpath))
                 print(line_hits)
