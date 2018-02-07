225e8f7067ded0872c43e2ba88655a7f27ce5d92
Kevin McGarry <kevinjmcgarry@gmail.com>
2018-02-07 13:21:00 -0600

    fixed column header line and added comments

--- file-related/pdf_report.py
+++ file-related/pdf_report.py
@@ -13,26 +13,35 @@ pip3 install pypdf2 (don't use original pypdf with python 3.x+)
 
 """
 
-import PyPDF2
+import PyPDF2  # https://github.com/mstamy2/PyPDF2
 import os
 
 
-start_dir = "/Users/kevinmcgarry/temp24/"
-file_path_dest = "/Users/kevinmcgarry/temp22/info.txt"
+tart_dir = "/Users/kevinmcgarry/temp24/"  # to be set as an argument
+file_path_dest = "/Users/kevinmcgarry/temp22/info.txt"  # to be set as an argument
 
 with open(file_path_dest, 'w') as outputfile:
-    outputfile.write("File Name\t Pages\t Creation Date\t Modified Date")  # column headers
+    outputfile.write("File Name\t Pages\t Creation Date\t Modified Date\n")  # column headers
     for current_dir, subdirs, files in os.walk(start_dir):
         # print(files)
         for each_file in files:
             full_path = os.path.join(current_dir, each_file)
+            relative_path = full_path[len(start_dir):]  # stripping off user path information
             if full_path.endswith("DS_Store"):
                 continue
-            print(full_path)
+            print(relative_path)
+            # print(full_path)
             reader = PyPDF2.PdfFileReader(open(full_path, 'rb'))
             doc_info = reader.getDocumentInfo()
             num_pages = reader.getNumPages()
             creation_date = doc_info['/CreationDate'][2:10]
             modified_date = doc_info['/ModDate'][2:10]
-            full_doc_info = f"{full_path} \t {num_pages} \t {creation_date} \t {modified_date} \n"
+            # substitute full_path in the f string first position for entire path
+            full_doc_info = f"{relative_path} \t {num_pages} \t {creation_date} \t {modified_date} \n"
             outputfile.write(full_doc_info)
+
+# The PDF-izer
+# would you like to index all pdfs found? y or n
+# would you like to print a report, y or no
+# starting dir location
+# where to store report file
