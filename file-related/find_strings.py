c86c790daad06e987c7156d554e2292ccf4da303
Kevin McGarry <kevinjmcgarry@gmail.com>
2018-04-02 16:50:12 -0500

    added exception handling and list sort output

--- file-related/find_strings.py
+++ file-related/find_strings.py
@@ -1,7 +1,6 @@
 '''
-Script used to recursively go through a folder and search each file for a specific string of interest.
-It then outputs in what file (and the number of lines in that file) that the string was found.
-Will update with exception handling.
+Script used to recursively go through a folder and search text files for a specific string of interest.
+It then outputs the name (including full path) of the file and the number of times the word was found.
 '''
 
 
@@ -12,20 +11,33 @@ def findstring():
 
     import os
 
+    file_hits_dict = {}
+    file_hits_list = []
+
     for this_dir, dir_names, file_names in os.walk(startDirectory):
         for eachFile in file_names:
             fpath = os.path.join(this_dir, eachFile)
-            with open(fpath, 'r') as inputfile:
-                count = 0  # reset count when opening new file
-                line_hits = []  # reset list when opening new file
-                for line in inputfile:
-                    words = line.split()
-                    for word in words:
-                        if word == wordOfInterest:
-                            count += 1
-                            line_hits.append(line)  # append all file's lines that contain the string
+            try:
+                with open(fpath, 'r') as inputfile:
+                    count = 0  # reset count when opening new file
+                    for line in inputfile:
+                        words = line.split()
+                        for word in words:
+                            if word == wordOfInterest:
+                                count += 1
+            except UnicodeDecodeError:
+                print(f"This utility won't work on the non-text file \"{fpath}\"")
             if count > 0:
-                print("\n found the word \'{}\' {} times in file \'{}\'".format(wordOfInterest, count, fpath))
-                print(line_hits)
+                file_hits_dict[fpath] = count  # creating a dictionary of files and word count for sorting
+    if len(file_hits_dict) > 0:
+        for k, v in file_hits_dict.items():
+            file_hits_list.append((v, k))
+        file_hits_sorted = sorted(file_hits_list, reverse=True)
+        print(f"\nFiles containing the word \"{wordOfInterest}\" sorted by number of times the word occurs --")
+        for hit in file_hits_sorted:
+            print(hit)
+    else:
+        print(f"no files were found containing the word \"{wordOfInterest}\"")
+
 
 findstring()
