1e405a01cc35cd1e707f7f5084d87df187a22aad
Kevin McGarry <kevinjmcgarry@gmail.com>
2018-04-02 18:49:50 -0500

    added logic to extract text from pdfs and generate sorted count

--- file-related/pdf_word_finder.py
+++ file-related/pdf_word_finder.py
@@ -1,35 +1,47 @@
-"""
-https://craigdanielmiller.com/2016/09/10/finding-words-with-pypdf2/
+'''
+Script used to recursively go through a folder and search pdf files for a specific string of interest.
+It then outputs the name of the file (including the full path) and the number of times the word was found in the file.
+'''
 
-This is a script for finding all instances of a given search word (or multiple search words) in a PDF.
 
-For our example, we’ll be using a PDF of Romeo and Juliet.  In this case, our search terms are “Romeo” and “Juliet” (search is not case-sensitive).
+def pdf_word_finder():
 
-"""
+    wordOfInterest = input('Enter the case-sensitive string you want to search for: ')
+    startDirectory = input('Enter the full path to the root folder that you want to start your search in: ')
 
-import PyPDF2
-import re
- 
-pdfFileObj=open(r'C:\Users\Craig\RomeoAndJuliet.pdf',mode='rb')
-pdfReader=PyPDF2.PdfFileReader(pdfFileObj)
-number_of_pages=pdfReader.numPages
- 
-pages_text=[]
-words_start_pos={}
-words={}
- 
-searchwords=['romeo','juliet']
- 
-with open('FoundWordsList.csv', 'w') as f:
-    f.write('{0},{1}\n'.format("Sheet Number", "Search Word"))
-    for word in searchwords:
-        for page in range(number_of_pages):
-            print(page)
-            pages_text.append(pdfReader.getPage(page).extractText())
-            words_start_pos[page]=[dwg.start() for dwg in re.finditer(word, pages_text[page].lower())]
-            words[page]=[pages_text[page][value:value+len(word)] for value in words_start_pos[page]]
-        for page in words:
-            for i in range(0,len(words[page])):
-               if str(words[page][i]) != 'nan':
-                    f.write('{0},{1}\n'.format(page+1, words[page][i]))
-                    print(page, words[page][i])
+    import PyPDF2
+    import os
+
+    file_hits_dict = {}
+    file_hits_list = []
+
+    for this_dir, dir_names, file_names in os.walk(startDirectory):
+        for eachFile in file_names:
+            if eachFile.lower().endswith(('.pdf')):
+                fpath = os.path.join(this_dir, eachFile)
+                with open(fpath, 'rb') as inputfile:
+                    count = 0
+                    pdfReader = PyPDF2.PdfFileReader(inputfile)
+                    number_of_pages = pdfReader.numPages
+                    for page in range(number_of_pages):
+                        print(page)
+                        extracted_text = pdfReader.getPage(page).extractText()
+                        words = extracted_text.split()
+                        print(words)
+                        for word in words:
+                            if word == wordOfInterest:
+                                count += 1
+                if count > 0:
+                    file_hits_dict[fpath] = count  # creating a dictionary of files and word count for sorting
+
+    if len(file_hits_dict) > 0:
+        for k, v in file_hits_dict.items():
+            file_hits_list.append((v, k))
+        file_hits_sorted = sorted(file_hits_list, reverse=True)
+        print(f"\nFiles containing the word \"{wordOfInterest}\" sorted by number of times the word occurs --")
+        for hit in file_hits_sorted:
+            print(hit)
+    else:
+        print(f"no files were found containing the word \"{wordOfInterest}\"")
+
+pdf_word_finder()
